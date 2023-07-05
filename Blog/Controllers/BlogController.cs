9e29779be702f0ef3294b5450f4971e1432ca7cd
Azizbek Askarov <azizasqarov773@gmail.com>
2023-07-05 17:30:08 +0500

    clean

--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult CreatorPage()
+        static List<BlogEntry> Posts = new List<BlogEntry>();
+        public IActionResult Index()
         {
-            //a
+
+            return View("Index", Posts);
+        }
+
+        public IActionResult CreatorPage(Guid id)
+        {
+            if (id != Guid.Empty)
+            {
+                BlogEntry existingEntry = Posts.FirstOrDefault(b => b.Id == id);
+
+                return View(model: existingEntry);
+            }
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatorPage(BlogEntry entry)
+        {
+            if (entry.Id == Guid.Empty)
+            {
+                var newEntry = new BlogEntry()
+                {
+                    Id = Guid.NewGuid(),
+                    Content = entry.Content,
+                };
+
+                Posts.Add(newEntry);
+            }
+            else
+            {
+                var existingEntry = Posts.FirstOrDefault(p => p.Id == entry.Id);
+                existingEntry.Content = entry.Content;
+            }
+
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Blog/Models/BlogEntry.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Models;
+
+public class BlogEntry
+{
+    public Guid Id { get; set; }
+    public string Content { get; set; }
+}
